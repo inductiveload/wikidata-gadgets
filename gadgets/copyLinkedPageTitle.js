fdbe603fccd2e4ceba9d228073eb1e8eb049ba1a
inductiveload <inductiveload@gmail.com>
2013-03-02 21:23:01 +0000

    Ctrl-click saves immediately

--- gadgets/copyLinkedPageTitle.js
+++ gadgets/copyLinkedPageTitle.js
@@ -9,35 +9,39 @@ var CopyLinkedPageTitle = (function ($) {
 	var my = {};
 
 	var tagIcon = '//upload.wikimedia.org/wikipedia/commons/1/1d/Tag_blue.png';
-	
+
 	var insertLink = function (i, v) {
 		$(v).append( $('<span>')
 				.attr({'class':'x-sitelinktolabel'})
 				.css({'padding-left':'3px'})
 				.append( $('<img>', {
-					'src':tagIcon, 
+					'src':tagIcon,
 					'title':'Use for the item label'})
 				.click(useLabel)
 				)
 			);
 	}
-	
+
 	var useLabel = function (e){
 		var label = $(e.target).parents('.wb-editsection').siblings('.wb-sitelinks-link').children('a').text();
-		
+
 		var labelBox = $('.wb-ui-labeledittool input');
-		
+
 		labelBox.val(label).trigger('input');
+
+		if (e.ctrlKey) {
+			$('.wb-ui-labeledittool a:contains(save)').click();
+		}
 	}
-	
-	
+
+
 	my.addLinks = function () {
-		
+
 		if (wgNamespaceNumber === 0 && $('.wb-ui-labeledittool input').length > 0){
 			$('.wb-sitelinks tbody .wb-editsection .wb-ui-toolbar').each( insertLink );
 		}
 	};
-	
+
 	return my;
 }(jQuery));
 
